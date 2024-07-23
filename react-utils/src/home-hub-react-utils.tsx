7e6066873c924a356812c3ab567a85d1e5875297
Pedro Mello <contato@cicatriz.dev>
2024-07-23 14:44:00 -0300

    codigo aula 3.1 do curso 4

--- react-utils/src/home-hub-react-utils.tsx
+++ react-utils/src/home-hub-react-utils.tsx
@@ -13,3 +13,22 @@ export function loginFunction(email: AuthInfo['email']) {
 	cookies.set('auth', { email: email, authId: authId });
 	location.replace(`/dashboard/${authId}/`);
 }
+
+export function logoutFunction() {
+	const cookies = new Cookies(null, { path: '/' });
+	cookies.remove('auth');
+	return location.replace('/');
+}
+
+export function checkIsAuthenticated() {
+	const cookies = new Cookies(null, { path: '/' });
+	const auth = cookies.get('auth');
+	if (!auth) {
+		return { authInfo: undefined, isAuthenticated: false };
+	}
+	const authObj: AuthInfo = JSON.parse(auth);
+	if (!location.pathname.includes(authObj.authId.toString())) {
+		return { authInfo: undefined, isAuthenticated: false };
+	}
+	return { authInfo: authObj, isAuthenticated: true };
+}
