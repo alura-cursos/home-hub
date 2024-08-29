a5187da38ed7b0177571fa8e378b74655d47efba
Pedro Mello <contato@cicatriz.dev>
2024-08-29 19:51:51 -0300

    codigo curso 5 aula 3.1

--- react-parcel/src/home-hub-react-parcel.tsx
+++ react-parcel/src/home-hub-react-parcel.tsx
@@ -1,4 +1,4 @@
-import ParcelComponent from './parcel.component';
+import Parcel from './parcel.component';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import singleSpaReact from 'single-spa-react';
@@ -6,7 +6,7 @@ import singleSpaReact from 'single-spa-react';
 const lifecycles = singleSpaReact({
 	React,
 	ReactDOM,
-	rootComponent: ParcelComponent,
+	rootComponent: Parcel,
 	errorBoundary(err, info, props) {
 		// Customize the root error boundary for your microfrontend here.
 		return null;
